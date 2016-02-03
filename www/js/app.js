bfb6cb02d1c70923e9b3e5831803fdad13c78896
Jay <imjching@hotmail.com>
2016-02-03 23:44:55 +0800

    IIFE

--- www/js/app.js
+++ www/js/app.js
@@ -1,108 +1,110 @@
-// Ionic Starter App
+(function(){
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngCordova'])
+  // angular.module is a global place for creating, registering and retrieving Angular modules
+  // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+  // the 2nd parameter is an array of 'requires'
+  // 'starter.controllers' is found in controllers.js
+  angular.module('starter', ['ionic', 'ngCordova'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html'
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
-  .state('app.dialogs', {
-    url: '/dialogs',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/dialogs.html',
-        controller: 'DialogsCtrl'
+
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/menu.html'
+    })
+    .state('app.dialogs', {
+      url: '/dialogs',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/dialogs.html',
+          controller: 'DialogsCtrl'
+        }
       }
-    }
-  }).state('app.device', {
-    url: '/device',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/device.html',
-        controller: 'DeviceCtrl'
+    }).state('app.device', {
+      url: '/device',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/device.html',
+          controller: 'DeviceCtrl'
+        }
       }
-    }
-  }).state('app.network', {
-    url: '/network',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/network.html',
-        controller: 'NetworkCtrl'
+    }).state('app.network', {
+      url: '/network',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/network.html',
+          controller: 'NetworkCtrl'
+        }
       }
-    }
-  }).state('app.vibration', {
-    url: '/vibration',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/vibration.html',
-        controller: 'VibrationCtrl'
+    }).state('app.vibration', {
+      url: '/vibration',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/vibration.html',
+          controller: 'VibrationCtrl'
+        }
       }
-    }
-  }).state('app.barcode_scanner', {
-    url: '/barcode_scanner',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/barcode_scanner.html',
-        controller: 'BarcodeScannerCtrl'
+    }).state('app.barcode_scanner', {
+      url: '/barcode_scanner',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/barcode_scanner.html',
+          controller: 'BarcodeScannerCtrl'
+        }
       }
-    }
-  }).state('app.camera', {
-    url: '/camera',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/camera.html',
-        controller: 'CameraCtrl'
+    }).state('app.camera', {
+      url: '/camera',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/camera.html',
+          controller: 'CameraCtrl'
+        }
       }
-    }
-  }).state('app.pin_dialog', {
-    url: '/pin_dialog',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/pin_dialog.html',
-        controller: 'PinDialogCtrl'
+    }).state('app.pin_dialog', {
+      url: '/pin_dialog',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/pin_dialog.html',
+          controller: 'PinDialogCtrl'
+        }
       }
-    }
-  }).state('app.touchid', {
-    url: '/touchid',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/touchid.html',
-        controller: 'TouchIdCtrl'
+    }).state('app.touchid', {
+      url: '/touchid',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/touchid.html',
+          controller: 'TouchIdCtrl'
+        }
       }
-    }
-  }).state('app.toast', {
-    url: '/toast',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/toast.html',
-        controller: 'ToastCtrl as vm'
+    }).state('app.toast', {
+      url: '/toast',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/toast.html',
+          controller: 'ToastCtrl as vm'
+        }
       }
-    }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/dialogs');
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/dialogs');
-});
+
+}());
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,212 +1,215 @@
-angular.module('starter')
+(function(){
 
-.controller('DialogsCtrl', function($cordovaDialogs, $scope) {
-  // REF: http://ngcordova.com/docs/plugins/dialogs/
+  angular.module('starter')
 
-  // alert(message, title, buttonName)
-  $scope.alert = function() {
-    $cordovaDialogs.alert('Wow!', 'Alert Title', 'Dismiss');
-  };
+  .controller('DialogsCtrl', function($cordovaDialogs, $scope) {
+    // REF: http://ngcordova.com/docs/plugins/dialogs/
 
-  // confirm(message, title, buttonArray)
-  $scope.confirm = function() {
-    $cordovaDialogs.confirm('Are you sure?', 'Confirmation', ['Yes', 'No'])
-      .then(function(buttonIndex) {
-        // no button = 0, 'Yes' = 1, 'No' = 2
-        $cordovaDialogs.alert('You selected button ' + buttonIndex, 'Response');
-      });
-  };
-
-  // prompt(message, title, buttonArray, defaultText)
-  $scope.prompt = function() {
-    $cordovaDialogs.prompt('Please Login', 'Login', ['Login', 'Cancel'], 'Change me')
-      .then(function(result) {
-        var input = result.input1;
-        // no button = 0, 'OK' = 1, 'Cancel' = 2
-        var btnIndex = result.buttonIndex;
-
-        $cordovaDialogs.alert('You selected button number ' + btnIndex + ' and entered ' + input, 'Response');
-      });
-  };
-
-  // beep(repetitions)
-  $scope.beep = function() {
-    // beep 1 time
-    $cordovaDialogs.beep(1);
-  };
-})
-
-.controller('DeviceCtrl', function($cordovaDevice, $scope) {
-  // REF: http://ngcordova.com/docs/plugins/device/
-
-  $scope.device = function() {
-    $scope.results = $cordovaDevice.getDevice();
-  };
-
-  $scope.cordovaVersion = function() {
-    $scope.results = $cordovaDevice.getCordova();
-  };
-
-  $scope.deviceModel = function() {
-    $scope.results = $cordovaDevice.getModel();
-  };
-
-  $scope.devicePlatform = function() {
-    $scope.results = $cordovaDevice.getPlatform();
-  };
-
-  $scope.deviceUUID = function(){
-    $scope.results = $cordovaDevice.getUUID();
-  };
-
-  $scope.deviceVersion = function() {
-    $scope.results = $cordovaDevice.getVersion();
-  };
-
-})
-
-.controller('NetworkCtrl', function($cordovaNetwork, $scope, $rootScope, $cordovaDialogs) {
-  // REF: http://ngcordova.com/docs/plugins/network/
-
-  $scope.type = $cordovaNetwork.getNetwork();
-
-  $scope.isOnline = $cordovaNetwork.isOnline();
-
-  $scope.isOffline = $cordovaNetwork.isOffline();
-
-  // listen for Online event
-  $rootScope.$on('$cordovaNetwork:online', function(event, networkState){
-    var onlineState = networkState;
-
-    $cordovaDialogs.alert('Online event is fired. Status: ' + onlineState, 'Fired!');
-  });
-
-  // listen for Offline event
-  $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
-    var offlineState = networkState;
-
-    $cordovaDialogs.alert('Offline event is fired. Status: ' + onlineState, 'Fired!');
-  });
-})
-
-.controller('VibrationCtrl', function($cordovaVibration, $scope) {
-  // REF: http://ngcordova.com/docs/plugins/vibration/
-
-  $scope.vibrate = function() {
-    $cordovaVibration.vibrate(500);
-  };
-})
-
-.controller('BarcodeScannerCtrl', function($cordovaBarcodeScanner, $scope, $cordovaDialogs) {
-  // REF: http://ngcordova.com/docs/plugins/barcodeScanner/
-
-  $scope.scan = function() {
-    $cordovaBarcodeScanner.scan().then(function(imageData) {
-      // Success! Barcode data is here
-      $scope.results = imageData;
-    }, function(err) {
-      // An error occured. Show a message to the user
-      $cordovaDialogs.alert('Error scanning: ' + err, 'Error!');
-    });
-  };
-
-  $scope.lookup = function() {
-    window.open("http://www.upcindex.com/" + $scope.results.text, "_system");
-  };
-
-  $scope.reset = function() {
-    $scope.results = null;
-  };
-})
-
-.controller('CameraCtrl', function($cordovaCamera, $scope, $cordovaDialogs) {
-  // REF: http://ngcordova.com/docs/plugins/camera/
-
-  $scope.takePicture = function() {
-    var options = {
-      quality : 100,
-      destinationType : Camera.DestinationType.DATA_URL,
-      sourceType : Camera.PictureSourceType.CAMERA,
-      allowEdit : true,
-      encodingType: Camera.EncodingType.JPEG,
-      targetWidth: 300,
-      targetHeight: 300,
-      popoverOptions: CameraPopoverOptions,
-      saveToPhotoAlbum: false,
-      correctOrientation: true
+    // alert(message, title, buttonName)
+    $scope.alert = function() {
+      $cordovaDialogs.alert('Wow!', 'Alert Title', 'Dismiss');
     };
 
-    $cordovaCamera.getPicture(options).then(function(imageData) {
-      // Success! Image data is here
-      $scope.imgSrc = "data:image/jpeg;base64," + imageData;
-    }, function(err) {
-      $cordovaDialogs.alert('An error occurred: ' + err, 'Error!');
-    });
-  };
-
-  $scope.selectPicture = function() {
-    var options = {
-      destinationType: Camera.DestinationType.FILE_URI,
-      sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+    // confirm(message, title, buttonArray)
+    $scope.confirm = function() {
+      $cordovaDialogs.confirm('Are you sure?', 'Confirmation', ['Yes', 'No'])
+        .then(function(buttonIndex) {
+          // no button = 0, 'Yes' = 1, 'No' = 2
+          $cordovaDialogs.alert('You selected button ' + buttonIndex, 'Response');
+        });
     };
 
-    $cordovaCamera.getPicture(options).then(function(imageUri) {
-      // Success! Image data is here
-      $scope.imgSrc = imageUri;
-    }, function(err) {
-      $cordovaDialogs.alert('An error occurred: ' + err, 'Error!');
+    // prompt(message, title, buttonArray, defaultText)
+    $scope.prompt = function() {
+      $cordovaDialogs.prompt('Please Login', 'Login', ['Login', 'Cancel'], 'Change me')
+        .then(function(result) {
+          var input = result.input1;
+          // no button = 0, 'OK' = 1, 'Cancel' = 2
+          var btnIndex = result.buttonIndex;
+
+          $cordovaDialogs.alert('You selected button number ' + btnIndex + ' and entered ' + input, 'Response');
+        });
+    };
+
+    // beep(repetitions)
+    $scope.beep = function() {
+      // beep 1 time
+      $cordovaDialogs.beep(1);
+    };
+  })
+
+  .controller('DeviceCtrl', function($cordovaDevice, $scope) {
+    // REF: http://ngcordova.com/docs/plugins/device/
+
+    $scope.device = function() {
+      $scope.results = $cordovaDevice.getDevice();
+    };
+
+    $scope.cordovaVersion = function() {
+      $scope.results = $cordovaDevice.getCordova();
+    };
+
+    $scope.deviceModel = function() {
+      $scope.results = $cordovaDevice.getModel();
+    };
+
+    $scope.devicePlatform = function() {
+      $scope.results = $cordovaDevice.getPlatform();
+    };
+
+    $scope.deviceUUID = function(){
+      $scope.results = $cordovaDevice.getUUID();
+    };
+
+    $scope.deviceVersion = function() {
+      $scope.results = $cordovaDevice.getVersion();
+    };
+
+  })
+
+  .controller('NetworkCtrl', function($cordovaNetwork, $scope, $rootScope, $cordovaDialogs) {
+    // REF: http://ngcordova.com/docs/plugins/network/
+
+    $scope.type = $cordovaNetwork.getNetwork();
+
+    $scope.isOnline = $cordovaNetwork.isOnline();
+
+    $scope.isOffline = $cordovaNetwork.isOffline();
+
+    // listen for Online event
+    $rootScope.$on('$cordovaNetwork:online', function(event, networkState){
+      var onlineState = networkState;
+
+      $cordovaDialogs.alert('Online event is fired. Status: ' + onlineState, 'Fired!');
     });
-  };
-})
 
-.controller('PinDialogCtrl', function($cordovaPinDialog, $scope, $cordovaDialogs) {
+    // listen for Offline event
+    $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
+      var offlineState = networkState;
 
-  $scope.request = function() {
-    $cordovaPinDialog.prompt('Enter your PIN').then(
-      function(result) {
-        $cordovaDialogs.alert('You have entered ' + JSON.stringify(result), 'Success');
-      },
-      function (error) {
-        $cordovaDialogs.alert('You have an error: ' + error, 'Error');
+      $cordovaDialogs.alert('Offline event is fired. Status: ' + onlineState, 'Fired!');
     });
-  };
+  })
 
-})
+  .controller('VibrationCtrl', function($cordovaVibration, $scope) {
+    // REF: http://ngcordova.com/docs/plugins/vibration/
 
-.controller('TouchIdCtrl', function($cordovaTouchID, $scope, $cordovaDialogs) {
+    $scope.vibrate = function() {
+      $cordovaVibration.vibrate(500);
+    };
+  })
 
-  $scope.checkSupport = function() {
-    $cordovaTouchID.checkSupport().then(function() {
-      // success, TouchID supported
-      $scope.results = 'TouchID supported';
-    }, function (error) {
-      $scope.results = 'TouchID not supported. Error: ' + error;
-    });
-  };
+  .controller('BarcodeScannerCtrl', function($cordovaBarcodeScanner, $scope, $cordovaDialogs) {
+    // REF: http://ngcordova.com/docs/plugins/barcodeScanner/
 
-  $scope.authenticate = function() {
-    $cordovaTouchID.authenticate("text").then(function() {
-      // success
-      $scope.results = 'Success!';
-    }, function () {
-      // error
-      $scope.results = 'Failed to authenticate';
-    });
-  };
+    $scope.scan = function() {
+      $cordovaBarcodeScanner.scan().then(function(imageData) {
+        // Success! Barcode data is here
+        $scope.results = imageData;
+      }, function(err) {
+        // An error occured. Show a message to the user
+        $cordovaDialogs.alert('Error scanning: ' + err, 'Error!');
+      });
+    };
 
-})
+    $scope.lookup = function() {
+      window.open("http://www.upcindex.com/" + $scope.results.text, "_system");
+    };
 
-.controller('ToastCtrl', function($cordovaToast) {
-  var vm = this;
+    $scope.reset = function() {
+      $scope.results = null;
+    };
+  })
 
-  vm.toast = function() {
-    $cordovaToast.show(vm.message, vm.duration, vm.position).then(function(success) {
-      // success
-      vm.results = 'Success: ' + success;
-    }, function (error) {
-      // error
-      vm.results = 'Error: ' + error;
-    });
-  };
+  .controller('CameraCtrl', function($cordovaCamera, $scope, $cordovaDialogs) {
+    // REF: http://ngcordova.com/docs/plugins/camera/
 
-});
+    $scope.takePicture = function() {
+      var options = {
+        quality : 100,
+        destinationType : Camera.DestinationType.DATA_URL,
+        sourceType : Camera.PictureSourceType.CAMERA,
+        allowEdit : true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 300,
+        targetHeight: 300,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false,
+        correctOrientation: true
+      };
+
+      $cordovaCamera.getPicture(options).then(function(imageData) {
+        // Success! Image data is here
+        $scope.imgSrc = "data:image/jpeg;base64," + imageData;
+      }, function(err) {
+        $cordovaDialogs.alert('An error occurred: ' + err, 'Error!');
+      });
+    };
+
+    $scope.selectPicture = function() {
+      var options = {
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+      };
+
+      $cordovaCamera.getPicture(options).then(function(imageUri) {
+        // Success! Image data is here
+        $scope.imgSrc = imageUri;
+      }, function(err) {
+        $cordovaDialogs.alert('An error occurred: ' + err, 'Error!');
+      });
+    };
+  })
+
+  .controller('PinDialogCtrl', function($cordovaPinDialog, $scope, $cordovaDialogs) {
+
+    $scope.request = function() {
+      $cordovaPinDialog.prompt('Enter your PIN').then(
+        function(result) {
+          $cordovaDialogs.alert('You have entered ' + JSON.stringify(result), 'Success');
+        },
+        function (error) {
+          $cordovaDialogs.alert('You have an error: ' + error, 'Error');
+      });
+    };
+
+  })
+
+  .controller('TouchIdCtrl', function($cordovaTouchID, $scope, $cordovaDialogs) {
+
+    $scope.checkSupport = function() {
+      $cordovaTouchID.checkSupport().then(function() {
+        // success, TouchID supported
+        $scope.results = 'TouchID supported';
+      }, function (error) {
+        $scope.results = 'TouchID not supported. Error: ' + error;
+      });
+    };
+
+    $scope.authenticate = function() {
+      $cordovaTouchID.authenticate("text").then(function() {
+        // success
+        $scope.results = 'Success!';
+      }, function () {
+        // error
+        $scope.results = 'Failed to authenticate';
+      });
+    };
+
+  })
+
+  .controller('ToastCtrl', function($cordovaToast) {
+    var vm = this;
+
+    vm.toast = function() {
+      $cordovaToast.show(vm.message, vm.duration, vm.position).then(function(success) {
+        // success
+        vm.results = 'Success: ' + success;
+      }, function (error) {
+        // error
+        vm.results = 'Error: ' + error;
+      });
+    };
+
+  });
+}());
